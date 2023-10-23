45f66300671e93e1bc10254c0b81eea0bae89e72
PurpleIllusions <148079714+PurpleIllusions@users.noreply.github.com>
2023-10-23 14:03:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -92,29 +92,6 @@ function playTrack() {
     playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
 }
 
-let searchInput = document.getElementById('search-input');
-
-function searchSongs() {
-    let query = searchInput.value.toLowerCase(); // Convert the search query to lowercase for case-insensitive search
-    let foundIndex = -1;
-
-    // Iterate through the music_list to find the matching song
-    for (let i = 0; i < music_list.length; i++) {
-        if (music_list[i].name.toLowerCase().includes(query) || music_list[i].artist.toLowerCase().includes(query)) {
-            foundIndex = i;
-            break;
-        }
-    }
-
-    if (foundIndex !== -1) {
-        // If a matching song is found, load and play it
-        loadTrack(foundIndex);
-        playTrack();
-    } else {
-        // Handle case when no matching song is found
-        alert('Song not found. Please try a different search.');
-    }
-}
 
 function pauseTrack() {
     curr_track.pause();
