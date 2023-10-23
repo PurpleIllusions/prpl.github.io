7c80183c7942eb905be1e753baacc1bb53b67775
PurpleIllusions <148079714+PurpleIllusions@users.noreply.github.com>
2023-10-23 15:13:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,28 +22,16 @@ let updateTimer;
 
 const music_list = [
     {
-        img : 'images/stay.png',
-        name : 'Stay',
-        artist : 'The Kid LAROI, Justin Bieber',
-        music : 'music/stay.mp3'
+        img: 'rip.jpg',
+        name: 'rip',
+        artist: 'carti',
+        music: 'rip.mp3'
     },
     {
-        img : 'images/fallingdown.jpg',
-        name : 'Falling Down',
-        artist : 'Wid Cards',
-        music : 'music/fallingdown.mp3'
-    },
-    {
-        img : 'images/faded.png',
-        name : 'Faded',
-        artist : 'Alan Walker',
-        music : 'music/Faded.mp3'
-    },
-    {
-        img : 'images/ratherbe.jpg',
-        name : 'Rather Be',
-        artist : 'Clean Bandit',
-        music : 'music/Rather Be.mp3'
+        img: 'rip.jpg',
+        name: 'over',
+        artist: 'carti',
+        music: 'over.mp3'
     }
 ];
 
