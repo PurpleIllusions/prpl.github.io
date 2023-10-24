2551424a15824fdbd158621e63082da779711d62
PurpleIllusions <148079714+PurpleIllusions@users.noreply.github.com>
2023-10-24 14:28:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function loadTrack(track_index){
     track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
     track_name.textContent = music_list[track_index].name;
     track_artist.textContent = music_list[track_index].artist;
-    now_playing.textContent = "Playing music " + (track_index + 1) + " of " + music_list.length;
+    now_playing.textContent = "Odtwarzanie " + (track_index + 1) + " z " + music_list.length + " piosenek";
 
     updateTimer = setInterval(setUpdate, 1000);
 
