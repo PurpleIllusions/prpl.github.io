553aefab7049cd3496e5b2d2a4681c3a635590c6
PurpleIllusions <148079714+PurpleIllusions@users.noreply.github.com>
2023-11-10 13:39:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let track_art = document.querySelector('.track-art');
 let track_name = document.querySelector('.track-name');
 let track_artist = document.querySelector('.track-artist');
 
-let playpause_btn = document.querySelector('.playpause-track');
-let next_btn = document.querySelector('.next-track');
-let prev_btn = document.querySelector('.prev-track');
+let playpause_btn = document.querySelector('.play');
+let next_btn = document.querySelector('.nastepny-utwor');
+let prev_btn = document.querySelector('.poprzedni-utowr');
 let seek_slider = document.querySelector('.seek_slider');
 let volume_slider = document.querySelector('.volume_slider');
 let curr_time = document.querySelector('.current-time');
