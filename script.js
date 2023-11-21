115c41e48774fb8b32bec41c2e58bfc23b751e00
PurpleIllusions <148079714+PurpleIllusions@users.noreply.github.com>
2023-11-21 20:44:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const lista_utworow = [
         name: 'i need u',
         artist: 'Ken Carson',
         song: 'muzyka/need u.mp3'
-    }.
+    },
     {
         img: 'jpgs/dielit.jpg',
         name: 'Rip',
