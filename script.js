812496bd7e7268c6638462e5546d93ce3dcc19cf
PurpleIllusions <jakubwojcik.2001@wp.pl>
2024-04-15 09:10:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,98 +1,154 @@
-let now_playing = document.querySelector('.now-playing');
-let okladkaUtw = document.querySelector('.okladka');
+let indexUtw = 0;
+let queue = [];
+let seek_slider = document.querySelector('.seek_slider');
+let vol_slider = document.querySelector('.vol_slider');
 let nazwaUtw = document.querySelector('.nazwa-utw');
 let nazwa_artysty = document.querySelector('.artysta');
 let play_button = document.querySelector('.play');
 let next = document.querySelector('.nastepny-utwor');
 let prev = document.querySelector('.poprzedni-utwor');
-let seek_slider = document.querySelector('.seek_slider');
-let vol_slider = document.querySelector('.vol_slider');
+let now_playing = document.querySelector('.now-playing');
+let okladkaUtw = document.querySelector('.okladka');
 let curr_time = document.querySelector('.current-time');
+let filtered_list = [];
 let total_duration = document.querySelector('.total-duration');
-let randomIcon = document.querySelector('.fa-shuffle');
+let random_icon = document.querySelector('.fa-shuffle');
 let curr_track = document.createElement('audio');
 let mute = false;
-let indexUtw = 0;
+let muteIcon = document.querySelector('.fa-volume-high');
 let isPlaying = false;
 let losowy_stan = false;
 let update_timer;
+let currentQueueIndex = 0;
 
-const lista_utworow = [
+load_song(indexUtw);
 
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/jpgs%2Fi_wonder.jpg?alt=media&token=04d8beb3-dcc1-446d-8300-1f7546a068bb',
-        name: 'I wonder',
-        artist: 'Kanye West',
-        song: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/muzyka%2FI_Wonder.mp3?alt=media&token=828848cd-8401-4fa7-921f-6cff6ffe15c9'
-    },
-    
-  {
-        img: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/jpgs%2Fdielit.jpg?alt=media&token=377257ef-0908-4ac7-a831-37aa4182ab1a',
-        name: 'Molly',
-        artist: 'Playboi Carti',
-        song: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/muzyka%2Fmolly.mp3?alt=media&token=7967afcc-ea9d-426a-8d8d-09b0c2f1dd9b'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/jpgs%2Fagc.jpg?alt=media&token=6836afed-42ed-49bb-9b8b-9597f3a46485',
-        name: 'Need u',
-        artist: 'Ken Carson',
-        song: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/muzyka%2Fneed_u.mp3?alt=media&token=43ae8704-2916-451c-99bc-0a1ad1cc5ad2'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/jpgs%2F2000.jpg?alt=media&token=b272927c-b787-480a-92a8-85a26c2c9071',
-        name: 'Show Me',
-        artist: 'Joey Bada$$',
-        song: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/muzyka%2FShow_me.mp3?alt=media&token=498832f0-151b-4348-bd47-851b25a3e72f'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/jpgs%2Fdielit.jpg?alt=media&token=377257ef-0908-4ac7-a831-37aa4182ab1a',
-        name: 'Rip',
-        artist: 'Playboi Carti',
-        song: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/muzyka%2Frip.mp3?alt=media&token=27dbf39e-4969-4b6a-9b91-57dcc9881845'
-    },
-    {
-        img: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/jpgs%2F2pac.jpg?alt=media&token=c826b538-04e2-4f45-9038-f75ead98283e',
-        name: 'Hit Em Up',
-        artist: '2Pac',
-        song: 'https://firebasestorage.googleapis.com/v0/b/odtwarzacz-b693f.appspot.com/o/muzyka%2FHit_Em_Up.mp3?alt=media&token=e4868b84-7b47-4eb8-b00e-da0ed2c0d149'
-    }
-];
-
-load_music(indexUtw);
-
-function load_music(indexUtw){
+async function load_song(indexUtw) {
     clearInterval(update_timer);
     reset();
+    try {
+        const collectionRef = firestore.collection('songs');
+        const querySnapshot = await collectionRef.get();
 
-    curr_track.src = lista_utworow[indexUtw].song;
-    curr_track.load();
+        if (querySnapshot.empty) {
+            console.log("brak utowrow w firestore");
+            return;
+        }
 
-    okladkaUtw.style.backgroundImage = "url(" + lista_utworow[indexUtw].img + ")";
-    nazwaUtw.textContent = lista_utworow[indexUtw].name;
-    nazwa_artysty.textContent = lista_utworow[indexUtw].artist;
-    now_playing.textContent = "Odtwarzanie " + (indexUtw + 1) + " z " + lista_utworow.length + " utworów";
+        indexUtw = (indexUtw + querySnapshot.size) % querySnapshot.size;
+        const docSnapshot = querySnapshot.docs[indexUtw];
+        const songData = docSnapshot.data();
 
-    update_timer = setInterval(setUpdate, 1000);
+        // jpg
+        const coverImageRef = storage.ref().child(songData.coverImage);
+        const coverImageUrl = await coverImageRef.getDownloadURL();
 
-    curr_track.addEventListener('ended', nextTrack);
-    
+        okladkaUtw.style.backgroundImage = "url(" + coverImageUrl + ")";
+        nazwaUtw.textContent = songData.name;
+        nazwa_artysty.textContent = songData.artist;
+        now_playing.textContent = "Odtwarzanie " + songData.name;
+
+        // mp3
+        const audioFileRef = storage.ref().child(songData.audioFile);
+        const songUrl = await audioFileRef.getDownloadURL();
+
+        curr_track.src = songUrl;
+        curr_track.load();
+
+        update_timer = setInterval(setUpdate, 1000);
+
+        curr_track.addEventListener('ended', next_song);
+    } catch (error) {
+        console.error("blad podczas ladowania utworu:", error);
+    }
 }
 
-let muteIcon = document.querySelector('.fa-volume-high');
 
-function toggleMute() {
-    if (mute) {
-        // Unmute the audio
-        curr_track.volume = vol_slider.value / 100;
-        mute = false;
-        muteIcon.classList.remove('fa-volume-mute');
-        muteIcon.classList.add('fa-volume-high');
-    } else {
-        // Mute the audio
-        curr_track.volume = 0;
-        mute = true;
-        muteIcon.classList.remove('fa-volume-high');
-        muteIcon.classList.add('fa-volume-mute');
+async function load_lib() {
+    try {
+        const collectionRef = firestore.collection('songs');
+        const querySnapshot = await collectionRef.get();
+        if (querySnapshot.empty) {
+            console.log("Brak piosenek w bazie");
+            return;
+        }
+        const lista_utwElement = document.getElementById('lista_utw');
+        lista_utwElement.innerHTML = "";
+
+        querySnapshot.forEach(doc => {
+            const songData = doc.data();
+            const listItem = document.createElement('li');
+            const add_to_que_icon = document.createElement('i');
+            add_to_que_icon.className = "fa-solid fa-bars-staggered add_to_que_icon";
+            add_to_que_icon.style.cursor = "pointer";
+            add_to_que_icon.addEventListener('click', () => add_to_que_from_lib(songData));
+            listItem.addEventListener('click', () => playClickedSong(songData));
+
+            listItem.textContent = `${songData.name} - ${songData.artist}`;
+
+         
+            listItem.appendChild(add_to_que_icon);
+            lista_utwElement.appendChild(listItem);
+        });
+    } catch (error) {
+        console.error("Błąd poczas ładowania listy:", error);
+    }
+}
+
+
+
+async function playClickedSong(songData) {
+    try {
+        // reset timer
+        clearInterval(update_timer);
+        reset();
+        const coverImageRef = storage.ref().child(songData.coverImage);
+        const coverImageUrl = await coverImageRef.getDownloadURL();
+
+        okladkaUtw.style.backgroundImage = "url(" + coverImageUrl + ")";
+        nazwaUtw.textContent = songData.name;
+        nazwa_artysty.textContent = songData.artist;
+        now_playing.textContent = "Odtwarzanie " + songData.name;
+
+        const audioFileRef = storage.ref().child(songData.audioFile);
+        const songUrl = await audioFileRef.getDownloadURL();
+
+        curr_track.src = songUrl;
+        curr_track.load();
+        update_timer = setInterval(setUpdate, 1000);
+            play_song();
+        
+    } catch (error) {
+        console.error("Blad funkcji playclicked:", error);
+    }
+}
+
+
+
+function add_to_que(songData) {
+    queue.push(songData);
+    console.log("Dodano do kolejki:", songData);
+
+  
+    if (!isPlaying) {
+        next_from_que();
+    }
+}
+
+
+window.onload = function () {
+    load_song(indexUtw);
+    load_lib();
+  
+};
+
+async function add_to_que_from_lib(songData) {
+    add_to_que(songData);
+}
+async function next_from_que() {
+    if (queue.length > 0) {
+        const nextSongData = queue.shift(); 
+        await update_player(nextSongData);
     }
 }
 
@@ -101,111 +157,232 @@ function reset(){
     total_duration.textContent = "00:00";
     seek_slider.value = 0;
 }
+
+function toggle_mute() {
+    if (mute) {
+        // Unmute
+        curr_track.volume = vol_slider.value / 100;
+        mute = false;
+        muteIcon.classList.remove('fa-volume-mute');
+        muteIcon.classList.add('fa-volume-high');
+    } else {
+        
+        curr_track.volume = 0;
+        mute = true;
+        muteIcon.classList.remove('fa-volume-high');
+        muteIcon.classList.add('fa-volume-mute');
+    }
+}
+
+
+
+
 function losowy_utwor(){
    losowy_stan ? stop_losowy() : play_losowy();
 }
+
 function play_losowy(){
    losowy_stan = true;
-    randomIcon.classList.add('randomActive');
-
-    
+    random_icon.classList.add('randomActive');
     var shuffleButton = document.querySelector('.shuffle-button');
     shuffleButton.style.color = '#229704'; 
     
 }
+
 function stop_losowy(){
     losowy_stan = false;
-    randomIcon.classList.remove('randomActive');
-
-   
+    random_icon.classList.remove('randomActive');
     var shuffleButton = document.querySelector('.shuffle-button');
     shuffleButton.style.color = '';
 }
-function repeatTrack(){
-    let current_index = indexUtw;
-    load_music(current_index);
-    playTrack();
+
+function repeat_song() {
+    if (isPlaying) {
+        curr_track.currentTime = 0;
+    } else {
+        let current_index = indexUtw;
+        load_song(current_index);
+        play_song();
+    }
 }
-function playpauseTrack(){
-    isPlaying ? pauseTrack() : playTrack();
+
+function playpause_song(){
+    isPlaying ? pause_song() : play_song();
 }
-function playTrack(){
+
+function play_song(){
     curr_track.play();
     isPlaying = true;
-
-   
-    play_button.innerHTML = '<i class="fa-solid fa-pause"></i>';
+    play_button.innerHTML = '<i class="fa-solid fa-pause"></i>'; 
 }
-function pauseTrack(){
+
+function pause_song(){
     curr_track.pause();
     isPlaying = false;
-   
-    
     play_button.innerHTML = '<i class="fa-solid fa-play"></i>';
 }
 
 
 
-// search
-function searchFunction() {
-    
+async function search_f() {
     var input = document.getElementById("search-input");
+    var searchText = input.value.toLowerCase();
 
-    
-    var searchText = input.value.toLowerCase(); 
+    try {
+        const collectionRef = firestore.collection('songs');
+        const querySnapshot = await collectionRef.get();
+     if (querySnapshot.empty) {
+            console.log("Brak piosenek");
+            return;
+        }
 
-  
-    var filteredMusicList = lista_utworow.filter(function (track) {
-        return (
-            track.name.toLowerCase().includes(searchText) ||
-            track.artist.toLowerCase().includes(searchText)
-        );
-    });
+        const filtered_list = querySnapshot.docs
+            .map(doc => doc.data())
+            .filter(songData =>
+                songData.name.toLowerCase().includes(searchText) ||
+                songData.artist.toLowerCase().includes(searchText)
+            );
 
-    if (filteredMusicList.length > 0) {
-        load_music(lista_utworow.indexOf(filteredMusicList[0]));
-        playTrack();
+        if (filtered_list.length > 0) {
+            const matchedSongData = filtered_list[0];
+            await update_player(matchedSongData);
+        }
+    } catch (error) {
+        console.error("Błąd wyszukiwania:", error);
     }
 
-  
     input.value = "";
 }
-
-
 document.getElementById("search-input").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-        searchFunction();
+        search_f();
     }
 });
 
 
-function nextTrack(){
-    if(indexUtw < lista_utworow.length - 1 && losowy_stan === false){
-        indexUtw += 1;
-    }else if(indexUtw < lista_utworow.length - 1 && losowy_stan === true){
-        let random_index = Number.parseInt(Math.random() * lista_utworow.length);
-        indexUtw = random_index;
-    }else{
-        indexUtw = 0;
+//NEXT AND PREV
+async function next_song() {
+    try {
+        if (queue.length > 0) {
+            currentQueueIndex = (currentQueueIndex + 1) % queue.length;
+            console.log("załadowano utwor o indexie", queue[currentQueueIndex]);
+            await update_player(queue[currentQueueIndex]);
+        } else if (losowy_stan) {
+            const collectionRef = firestore.collection('songs');
+            const querySnapshot = await collectionRef.get();
+
+            if (querySnapshot.empty) {
+                console.log("nie znaleziono piosenek");
+                return;
+            }
+
+            const randomIndex = getRandomIndex(querySnapshot.size);
+            const docSnapshot = querySnapshot.docs[randomIndex];
+            const randomSongData = docSnapshot.data();
+
+            await update_player(randomSongData);
+        } else {
+            const collectionRef = firestore.collection('songs');
+            const querySnapshot = await collectionRef.get();
+
+            if (querySnapshot.empty) {
+                console.log("nie znaleziono piosenek");
+                return;
+            }
+
+            indexUtw = (indexUtw + 1) % querySnapshot.size;
+            const docSnapshot = querySnapshot.docs[indexUtw];
+            const songData = docSnapshot.data();
+
+            await update_player(songData);
+        }
+    } catch (error) {
+        console.error("blad podczas ładowania utworu:", error);
     }
-    load_music(indexUtw);
-    playTrack();
 }
-function prevTrack(){
-    if(indexUtw > 0){
-        indexUtw -= 1;
-    }else{
-        indexUtw = lista_utworow.length -1;
+
+async function prev_song() {
+    try {
+        if (losowy_stan) {
+            // tryb shuffle
+            const collectionRef = firestore.collection('songs');
+            const querySnapshot = await collectionRef.get();
+
+            if (querySnapshot.empty) {
+                console.log("No songs found");
+                return;
+            }
+
+            const randomIndex = getRandomIndex(querySnapshot.size);
+            const docSnapshot = querySnapshot.docs[randomIndex];
+            const randomSongData = docSnapshot.data();
+
+            await update_player(randomSongData);
+        } else if (queue.length > 0) {
+           
+            currentQueueIndex = (currentQueueIndex - 1 + queue.length) % queue.length;
+            await update_player(queue[currentQueueIndex]);
+        } else {
+            
+            const collectionRef = firestore.collection('songs');
+            const querySnapshot = await collectionRef.get();
+
+            if (querySnapshot.empty) {
+                console.log("Brak utworow");
+                return;
+            }
+
+            indexUtw = (indexUtw - 1 + querySnapshot.size) % querySnapshot.size;
+            const docSnapshot = querySnapshot.docs[indexUtw];
+            const songData = docSnapshot.data();
+
+            await update_player(songData);
+        }
+    } catch (error) {
+        console.error("blad prev track:", error);
     }
-    load_music(indexUtw);
-    playTrack();
 }
-function seekTo(){
-    let seekto = curr_track.duration * (seek_slider.value / 100);
-    curr_track.currentTime = seekto;
+
+
+
+
+
+
+function getRandomIndex(collectionSize) {
+    return Math.floor(Math.random() * collectionSize);
 }
-function setVolume() {
-    // Ensure the volume is set to 0 when the slider is at 0
+
+async function update_player(songData) {
+    clearInterval(update_timer);
+    reset();
+    const coverImageRef = storage.ref().child(songData.coverImage);
+    const coverImageUrl = await coverImageRef.getDownloadURL();
+
+    okladkaUtw.style.backgroundImage = "url(" + coverImageUrl + ")";
+    nazwaUtw.textContent = songData.name;
+    nazwa_artysty.textContent = songData.artist;
+    now_playing.textContent = "Odtwarzanie " + songData.name;
+
+    const audioFileRef = storage.ref().child(songData.audioFile);
+    const songUrl = await audioFileRef.getDownloadURL();
+
+    curr_track.src = songUrl;
+    curr_track.load();
+
+    update_timer = setInterval(setUpdate, 1000);
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+    play_song();
+}
+
+
+
+function przewin(){
+    let przewin = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = przewin;
+}
+function set_vol() {
+  
     if (vol_slider.value == 0) {
         curr_track.volume = 0;
         mute = true;
@@ -214,6 +391,7 @@ function setVolume() {
         mute = false;
     }
 }
+
 function setUpdate(){
     let seekPosition = 0;
     if(!isNaN(curr_track.duration)){
@@ -234,3 +412,7 @@ function setUpdate(){
         total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
+
+
+
+
